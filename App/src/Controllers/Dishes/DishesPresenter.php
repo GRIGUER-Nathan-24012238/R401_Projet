a0fb58c479c4b6eb26204756b84278d4877fe760
Griguer Nathan <nathan.griguer@etu.univ-amu.fr>
2026-04-09 21:36:42 +0200

    New: added DishesPresenter

---
+++ App/src/Controllers/Dishes/DishesPresenter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\src\Controllers\Dishes;
+
+use App\src\Models\UseCase\Dishes\GetDishesUseCase;
+
+class DishesPresenter 
+{
+    private GetDishesUseCase $getDishesService;
+
+    public function __construct(GetDishesUseCase $getDishesService)
+    {
+        $this->getDishesService = $getDishesService;
+    }
+
+
+    public function present(): array
+    {
+        $dishes = $this->getDishesService->execute();
+        
+        $viewModels = [];
+
+        foreach ($dishes as $item) {
+            $viewModels[] = [
+                'nom' => htmlspecialchars($item->getName()),
+                'description' => htmlspecialchars($item->getDescription()),
+                'prix' => number_format($item->getPrix(), 2, ',', ' ') . ' €'
+            ];
+        }
+
+        return $viewModels;
+    }
+}
